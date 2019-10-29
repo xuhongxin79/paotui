999356c1031627cbfa87dbc8a580eff7534b3c13
xuhongxin79 <46701281+xuhongxin79@users.noreply.github.com>
2019-10-29 11:22:31 +0800

    Create nav.php

---
+++ hs/php/nav.php
@@ -0,0 +1,17 @@
+<!--创建导航条-->
+<div class="nav" id="nav">
+<a href="?f=main">
+首页
+</a>  
+<a href="?f=txl">
+通信
+</a>  
+<a href="?f=paoTuiSay&n25=14">
+发布
+</a>  
+<a href="?f=fx">
+发现</a>  
+<a href="?f=my">
+我的
+</a> 
+</div>
